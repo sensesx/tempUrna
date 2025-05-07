1977b1e35a27a945fdf30f9260d6c04ae45a6d62
vmouraz <morphine2002@outlook.com>
2025-05-07 18:38:12 -0300

    some changes

--- Urna/obj/Debug/net8.0-windows/Urna.AssemblyInfo.cs
+++ Urna/obj/Debug/net8.0-windows/Urna.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Urna")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d20222699d2da48e2b14cde4bfd9047cb443bfaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Urna")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Urna")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
